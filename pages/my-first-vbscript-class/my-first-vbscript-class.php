4004dc494edc4615fd69ec56326be915e0587ac6
Joe Crawford <joe@artlung.com>
2024-12-06 14:32:47 -0800

    use correct formatting on asp page

--- pages/my-first-vbscript-class/my-first-vbscript-class.php
+++ pages/my-first-vbscript-class/my-first-vbscript-class.php
@@ -13,25 +13,19 @@ My First ASP Class
 
     <h1>My first VBScript Test Class</h1>
 
-    <p><code class="pre">
-            <?php
-            print nl2br(htmlentities(file_get_contents("TestClass.asp")));
-            ?>
-        </code></p>
 
+<?php $lab->printCodeBlock(file_get_contents('TestClass.asp')); ?>
 
     <h2>And testing it out</h2>
 
 
-    <p><code class="pre">
-            <?php
-            print nl2br(htmlentities(file_get_contents("testing.asp")));
-            ?>
-        </code></p>
+<?php $lab->printCodeBlock(file_get_contents('testing.asp')); ?>
 
     <h2>Results</h2>
 
+<div style="border: solid;padding: 1rem">
 <?php require "results.html"; ?>
+</div>
 
 <?php
 $lab->printFooter(
