d384355dc8e782e24b75385c8b79c04fa35e811f
Joe Crawford <joe@artlung.com>
2024-12-22 19:39:52 -0800

    Webmention.io Puller class, with the json data along with it. Updated NAV. Plus script to run

---
+++ classes/WebmentionIoGetter.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace ArtlungLab;
+/**
+ * Class WebmentionIoGetter
+ *
+ * @category PHP
+ * @package  Classes
+ * @author   Joe Crawford <joe@artlung.com>
+ * @license  GPL 2.0+ - https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @version  Release: 1.0
+ * @link     https://artlung.com/
+ * @since    2024-12-03
+ */
+class WebmentionIoGetter
+{
+    const WEBMENTION_LAST_CHECKED_KEY = 'webmention_last_checked';
+    const WEBMENTION_COUNT_KEY = 'webmention_count';
+
+    /**
+     * Get webmentions for a URL
+     *
+     * @param string $url URL to get webmentions for
+     *
+     * @return array
+     */
+    public static function getWebmentions($url): array
+    {
+        $webmentionio_url = "https://webmention.io/api/mentions.jf2?target=$url";
+        $webmentionio_response = file_get_contents($webmentionio_url);
+        $webmentionio_data = json_decode($webmentionio_response, true);
+        return $webmentionio_data;
+    }
+
+    /**
+     * Get webmentions for a page
+     *
+     * @param string $url URL to get webmentions for
+     *
+     * @return array
+     */
+    public static function getWebmentionsForPage($url): array
+    {
+        $webmentionio_data = self::getWebmentions($url);
+        $webmentions = $webmentionio_data['children'] ?? [];
+        return $webmentions;
+    }
+
+    /**
+     * Get and stash webmentions
+     *
+     * @param string $slug Slug
+     * @param string $url  URL
+     *
+     * @return int
+     */
+    public static function getAndStashWebmentions($slug, $url): bool
+    {
+        $webmentions = self::getWebmentionsForPage($url);
+        $webmentions_json = json_encode($webmentions);
+        $webmentions_file = __DIR__ . "/../webmention_data/$slug.json";
+        $worked = file_put_contents($webmentions_file, $webmentions_json);
+        return (bool) $worked;
+    }
+
+
+
+}
---
+++ get-webmentions.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once 'loader.php';
+
+$pages = \ArtlungLab\Nav::getMetadata();
+
+
+foreach ($pages as $slug => $metadata) {
+
+    echo "Checking $slug\n";
+
+    $webmentions_file = "webmention_data/$slug.json";
+    $yaml_file = "web/$slug/$slug.yaml";
+
+    if (!file_exists($yaml_file)) {
+        print "No YAML file for $slug, it's not a real page yet, skipping...";
+        continue;
+    }
+
+    $yaml = Spyc::YAMLLoad($yaml_file);
+
+    if (isset($yaml[\ArtlungLab\WebmentionIoGetter::WEBMENTION_LAST_CHECKED_KEY])) {
+        $last_checked = $yaml[\ArtlungLab\WebmentionIoGetter::WEBMENTION_LAST_CHECKED_KEY];
+        print "Last checked date for $slug is $last_checked\n";
+
+        if (is_file($webmentions_file)) {
+            print  "There is a webmentions file for $slug\n";
+            $webmentions = json_decode(file_get_contents($webmentions_file), true);
+            $yaml[\ArtlungLab\WebmentionIoGetter::WEBMENTION_COUNT_KEY] = count($webmentions);
+            print "Count for $slug is " . count($webmentions) . "\n";
+            $yaml = Spyc::YAMLDump($yaml);
+            file_put_contents($yaml_file, $yaml);
+        }
+
+
+    } else {
+        $last_checked = 0;
+        print "No last checked date for $slug, setting to 0\n";
+    }
+
+
+
+
+    // only get them once a week
+    if (time() - $last_checked < 604800) {
+        print "Skipping $slug, last checked $last_checked\n";
+        continue;
+    }
+
+    $success = \ArtlungLab\WebmentionIoGetter::getAndStashWebmentions($slug, $metadata['canonical_url']);
+
+    if (!$success) {
+        print "Failed to get and stash webmentions for $slug\n";
+        continue;
+    }
+
+    $yaml[\ArtlungLab\WebmentionIoGetter::WEBMENTION_LAST_CHECKED_KEY] = time();
+
+    $yaml = Spyc::YAMLDump($yaml);
+
+    file_put_contents($yaml_file, $yaml);
+
+    print "Updated $slug\n";
+
+
+
+
+
+
+}
+
+echo "Done. If the webmention count changed,\n";
+echo "be sure to run generate-nav-value.php\n";
+echo "and composer snifferfix\n";
