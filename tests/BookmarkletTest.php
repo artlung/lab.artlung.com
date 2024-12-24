2fac69565475146433e8bfc60fb52faaf327b60a
Joe Crawford <joe@artlung.com>
2024-12-23 22:51:46 -0800

    Comments recommended by Snifferfix

--- tests/BookmarkletTest.php
+++ tests/BookmarkletTest.php
@@ -4,8 +4,25 @@
 use ArtlungLab\Bookmarklet;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class BookmarkletTest
+ *
+ * @category PHP
+ * @package  Tests
+ * @author   Joe Crawford <joe@artlung.com>
+ * @license  GPL 2.0+ - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @version  Release: 1.0
+ * @link     https://artlung.com/
+ * @since    2024-12-03
+ */
 class BookmarkletTest extends TestCase
 {
+    /**
+     * Test the javascriptToBookmarklet method
+     *
+     * @return void
+     * @throws Exception
+     */
     public function testJavascriptToBookmarklet()
     {
         $js = 'console.log("Hello, World!");';
