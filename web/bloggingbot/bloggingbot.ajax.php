54e305850cd5bd05c2277c044659f305155352f6
Joe Crawford <joe@artlung.com>
2024-12-28 05:55:45 -0800

    Numeric conditional fix

--- web/bloggingbot/bloggingbot.ajax.php
+++ web/bloggingbot/bloggingbot.ajax.php
@@ -5,15 +5,15 @@ require_once '../../loader.php';
 if (isset($_GET['id'])) {
     $id = (int)$_GET['id'];
     // if not 1 to 10000, error 404
-    if ($id >= 0 || $id >= 9999) {
+    if ($id < 0 || $id > 9999) {
         header('HTTP/1.0 404 Not Found');
         exit;
     }
     $sentences = file("sample_sentences.txt");
     $line = $sentences[$id];
 
-    // json
     header('Content-Type: application/json');
+    header('Cache-Control: max-age=31536000');
     print json_encode(
         [
         'itemId' => $id,
